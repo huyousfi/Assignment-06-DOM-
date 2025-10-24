f69cd0b0ed2a69ca7114e69c4e532df6486f5624
huyousfi <huyousfi@gmail.com>
2025-10-24 19:57:31 +0500

    starts remaining questions

---
+++ app.js
@@ -0,0 +1,4 @@
+const headingone = document.querySelector("#h-one");
+headingone.innerHTML = "How to select an html element in javascript"
+const paraone = document.querySelector("#p-one");
+paraone.innerHTML = "Definition Calling (or selecting) a heading or paragraph in JavaScript means accessing an HTML element like &quoth1, h2, or p&quot; from your web page so you can read, change, or manipulate it using JavaScript."
